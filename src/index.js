d6b7b519798e8c7a6b1a08c536e98814f191698b
Sourav Biswal <souravnitrkl611@gmail.com>
2025-11-17 22:24:24 +0530

    fix: cors uri

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://leetcode-clone-umber-ten.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allowed methods
 allowedHeaders: ["Content-Type", "Authorization"]     // headers frontend may send
